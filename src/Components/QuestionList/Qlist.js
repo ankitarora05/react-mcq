d8e2935c43d003a7c9479f20f73cd3d54c6d4405
Ankit Arora <05.ankitarora@gmail.com>
2018-02-25 19:25:32 +0530

    Changes for questions-list page

--- src/Components/QuestionList/Qlist.js
+++ src/Components/QuestionList/Qlist.js
@@ -1,13 +1,189 @@
 import React, { Component } from 'react';
 
 class QuestionList extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Main QuestionList Component</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="Main-question-list">
+                <div className="title">Question List</div>
+                <div className="Question-select-box">
+                    <div className="Header-bar">
+                        <div className="selection-box">
+                            <label>
+                                <input type="checkbox" name="login-form" value="selectAll" />
+                                Select All
+                                </label>
+                        </div>
+                        <div className="assign-box">
+                            <div className="assign-text">Select questions to Assign</div>
+                            <div className="Button-container">
+                                <button className="Primary-btn mar-1">Assign</button>
+                                <button className="Secondary-btn">Author New Question</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="Selection-rows">
+                        <div className="selection-check">
+                            <label><input type="checkbox" name="login-form" value="" /></label>
+                        </div>
+                        <div className="course-info-bar">
+                            <div className="serial-col">
+                                <div className="title">S.No.</div>
+                                <div className="font-16">1</div>
+                            </div>
+                            <div className="question-col">
+                                <div className="title">Creating External Table with partition and adding data in it.</div>
+                                <div className="sub-title">Why there are no airplanes visible in the Google Maps satellite view?</div>
+                            </div>
+                            <div className="question-type-col">
+                                <div className="title">Question Type</div>
+                                <div className="sub-title">Submission</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default QuestionList;
