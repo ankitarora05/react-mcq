9d80a7bc8e7de45cb0f8cddb0785afeb2f4f35f4
Ankit Arora <05.ankitarora@gmail.com>
2018-02-25 11:30:16 +0530

    Added 404 routing

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Header from './Components/Header/Header';
 import Login from './Components/Login/Login.js';
 import QuestionList from './Components/QuestionList/Qlist.js';
@@ -8,20 +8,24 @@ import CreateMCQ from './Components/CreateMCQ/Create.js';
 import AnswerQuestion from './Components/AnswerQuestion/AQuestion.js';
 import CreateSubmission from './Components/CreateSubmission/CSubmission.js';
 import CreateTextQuestion from './Components/CreateTextQuestion/CTQuestion.js';
+import ErrorComponent from './Components/NotFound/404.js';
 
 const Routes = (props) => (
-  <Router {...props}>
-    <div>
-        <Header />
-        <Route path="/Login" component={Login} />
-        <Route path="/QList" component={QuestionList} />
-        <Route path="/SList" component={StudentList} />
-        <Route path="/CreateMCQ" component={CreateMCQ} />
-        <Route path="/AnswerQuestion" component={AnswerQuestion} />
-        <Route path="/CreateSubmission" component={CreateSubmission} />
-        <Route path="/CreateTextQuestion" component={CreateTextQuestion} />
-    </div>
-  </Router>
+    <Router {...props}>
+        <div>
+            <Header />
+            <Switch>
+                <Route path="/Login" component={Login} />
+                <Route path="/QList" component={QuestionList} />
+                <Route path="/SList" component={StudentList} />
+                <Route path="/CreateMCQ" component={CreateMCQ} />
+                <Route path="/AnswerQuestion" component={AnswerQuestion} />
+                <Route path="/CreateSubmission" component={CreateSubmission} />
+                <Route path="/CreateTextQuestion" component={CreateTextQuestion} />
+                <Route component={ErrorComponent} />
+            </Switch>
+        </div>
+    </Router>
 );
 
 export default Routes;
